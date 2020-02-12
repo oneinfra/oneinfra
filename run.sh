6c50edfea243ac4d7c939be143d8670da733c50a
Rafael Fernández López <ereslibre@ereslibre.es>
2020-02-12 11:59:34 +0100

    Share go build cache throughout steps for faster execution

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -v $PWD:/app $EXTRA_FLAGS -v $PWD/bin:/go/bin -w /app -e GOFLAGS="-mod=vendor" oneinfra/builder:latest "$@"
+docker run --rm -v $PWD:/app -v $PWD/bin:/go/bin -v /tmp/go-build-cache:/root/.cache/go-build $EXTRA_FLAGS -w /app -e GOFLAGS="-mod=vendor" oneinfra/builder:latest "$@"

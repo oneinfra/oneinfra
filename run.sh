89a55b8a239d7e14e3ded1b270f8ab6ce879e0ec
Rafael Fernández López <ereslibre@ereslibre.es>
2020-02-12 12:17:28 +0100

    Change wrapper for faster local development

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-docker run --rm -v $PWD:/app -v $PWD/bin:/go/bin -v /tmp/go-build-cache:/root/.cache/go-build $EXTRA_FLAGS -w /app -e GOFLAGS="-mod=vendor" oneinfra/builder:latest "$@"
+if [ -z "$CI" ]; then
+    "$@"
+else
+    docker run --rm -v $PWD:/app -v $PWD/bin:/go/bin -v /tmp/go-build-cache:/root/.cache/go-build $EXTRA_FLAGS -w /app -e GOFLAGS="-mod=vendor" oneinfra/builder:latest "$@"
+fi

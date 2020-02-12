4b1de1d4375a34288dc05fb86846a0da785992f3
Rafael Fernández López <ereslibre@ereslibre.es>
2020-02-12 11:44:26 +0100

    Remove interactivity and tty from run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -v $PWD:/app $EXTRA_FLAGS -v $PWD/bin:/go/bin -w /app -e GOFLAGS="-mod=vendor" -it oneinfra/builder:latest "$@"
+docker run --rm -v $PWD:/app $EXTRA_FLAGS -v $PWD/bin:/go/bin -w /app -e GOFLAGS="-mod=vendor" oneinfra/builder:latest "$@"

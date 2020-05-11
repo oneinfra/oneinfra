ad3220d4756c346ffc24c9d2ecb5527a0be89d94
Rafael Fernández López <ereslibre@ereslibre.es>
2020-05-11 15:02:53 +0200

    fake workers: fix so they work with specific docker networks
    Fake workers are usually started in a development environment where
    kind is used as well, just make them play nice with the specific
    docker bridge network created by kind by default, if it exists.

--- scripts/create-fake-worker.sh
+++ scripts/create-fake-worker.sh
@@ -28,11 +28,27 @@ if [ -z "${CLUSTER_NAME}" ]; then
     exit 1
 fi
 
+NETWORK_ARG=""
+if [ -n "${NETWORK_NAME}" ]; then
+    if ! docker network inspect "${NETWORK_NAME}" &> /dev/null; then
+        echo "Network name ${NETWORK_NAME} not found, please set an existing network name or leave it blank"
+        exit 1
+    fi
+else
+    if docker network inspect kind &> /dev/null; then
+        NETWORK_NAME="kind"
+    fi
+fi
+
+if [ -n "${NETWORK_NAME}" ]; then
+    NETWORK_ARG="--network=${NETWORK_NAME}"
+fi
+
 OI_BIN=$(which oi)
 CONTAINERD_LOCAL_ENDPOINT="unix:///containerd-socket/containerd.sock"
 APISERVER_ENDPOINT=$(cat ${CLUSTER_CONF} | oi-local-hypervisor-set endpoint --cluster ${CLUSTER_NAME})
 CONTAINERD_VERSION=$(cat ${CLUSTER_CONF} | oi cluster version --cluster ${CLUSTER_NAME} component --component containerd)
-CONTAINER_ID=$(docker run --privileged -v /dev/null:/proc/swaps:ro -v ${OI_BIN}:/usr/local/bin/oi:ro -v $(realpath "${CLUSTER_CONF}"):/etc/oneinfra/cluster.conf:ro -d oneinfra/containerd:${CONTAINERD_VERSION})
+CONTAINER_ID=$(docker run --privileged ${NETWORK_ARG} -v /dev/null:/proc/swaps:ro -v /etc/resolv.conf:/etc/resolv.conf:ro -v ${OI_BIN}:/usr/local/bin/oi:ro -v $(realpath "${CLUSTER_CONF}"):/etc/oneinfra/cluster.conf:ro -d oneinfra/containerd:${CONTAINERD_VERSION})
 
 docker exec ${CONTAINER_ID} sh -c "rm /etc/cni/net.d/*"
 
@@ -46,6 +62,9 @@ cat "${CLUSTER_CONF}.new" | oi reconcile > ${CLUSTER_CONF} 2> /dev/null
 # Get the IP address of the fake worker, so we can request a SAN for
 # its kubelet server certificate
 FAKE_WORKER_IP_SAN=$(docker inspect ${CONTAINER_ID} | jq -r '.[0].NetworkSettings.IPAddress')
+if [ -z "${FAKE_WORKER_IP_SAN}" ]; then
+    FAKE_WORKER_IP_SAN=$(docker inspect ${CONTAINER_ID} | jq -r ".[0].NetworkSettings.Networks.${NETWORK_NAME}.IPAddress")
+fi
 
 echo "joining new node in background"
 docker exec ${CONTAINER_ID} sh -c "cat /etc/oneinfra/cluster.conf | oi cluster apiserver-ca --cluster ${CLUSTER_NAME} > /etc/oneinfra/apiserver-ca.crt"

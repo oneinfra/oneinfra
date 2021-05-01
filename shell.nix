3d4ac05ed5755a1408957ad0b39518212ea02952
Rafael Fernández López <ereslibre@ereslibre.es>
2021-05-01 16:57:08 +0200

    Set go 1.16 as the compiler

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@ pkgs.mkShell {
   buildInputs = [
     (import ./nix/kubebuilder.nix { inherit pkgs; })
     (import ./nix/controller-tools.nix { inherit pkgs; })
+    pkgs.go_1_16
     pkgs.golint
     pkgs.kustomize
     pkgs.jq

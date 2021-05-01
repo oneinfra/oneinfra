4d166cf915baf8d1fd744b61b409f11a0b674c0d
Rafael Fernández López <ereslibre@ereslibre.es>
2021-05-01 16:38:10 +0200

    Add golint to the nix shell

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@ pkgs.mkShell {
   buildInputs = [
     (import ./nix/kubebuilder.nix { inherit pkgs; })
     (import ./nix/controller-tools.nix { inherit pkgs; })
+    pkgs.golint
     pkgs.kustomize
     pkgs.jq
     pkgs.yq-go

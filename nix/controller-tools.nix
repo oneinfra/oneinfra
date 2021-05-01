64df0771fbb04e7329c07150c0ce1de4b70122df
Rafael Fernández López <ereslibre@ereslibre.es>
2021-05-02 00:26:55 +0200

    Update nix definitions

--- nix/controller-tools.nix
+++ nix/controller-tools.nix
@@ -1,10 +1,11 @@
 { pkgs }:
 let
+  name = "controller-tools";
   controllerToolsVersion = "0.5.0";
   sha256 = "169nljr5718hlww7d21cq853qmjiszkfl9mxx1j1k7bdl4nps2vq";
   vendorSha256 = "0xgrs0b3z9pwa57bh24nhmm3rp2wi0ynhxjwyp6zsqas6snnxnzc";
 in pkgs.buildGoModule {
-  pname = "controller-tools";
+  pname = name;
   version = controllerToolsVersion;
 
   vendorSha256 = vendorSha256;
@@ -15,7 +16,7 @@ in pkgs.buildGoModule {
 
   src = pkgs.fetchFromGitHub {
     owner = "kubernetes-sigs";
-    repo = "controller-tools";
+    repo = name;
     rev = "v${controllerToolsVersion}";
     sha256 = sha256;
   };
--- nix/kubebuilder.nix
+++ nix/kubebuilder.nix
@@ -1,10 +1,11 @@
 { pkgs }:
 let
+  name = "kubebuilder";
   kubebuilderVersion = "2.3.2";
   sha256 = "10f48nmpkb3kx36x92a77mnrn48y6fvwq9dxlfw0r35hsrv1sm2g";
   vendorSha256 = "1v0ba2h1ld8l4kvsd0rajl2540v98pa8q19lwbxdll28rz83msh9";
 in pkgs.buildGoModule {
-  pname = "kubebuilder";
+  pname = name;
   version = kubebuilderVersion;
 
   vendorSha256 = vendorSha256;
@@ -15,7 +16,7 @@ in pkgs.buildGoModule {
 
   src = pkgs.fetchFromGitHub {
     owner = "kubernetes-sigs";
-    repo = "kubebuilder";
+    repo = name;
     rev = "v${kubebuilderVersion}";
     sha256 = sha256;
   };

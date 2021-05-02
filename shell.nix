d8a068db57cd89a7d4e5a4a8632c1899ae87ea5b
Rafael Fernández López <ereslibre@ereslibre.es>
2021-05-02 12:53:10 +0200

    Pin nixpkgs revision

--- shell.nix
+++ shell.nix
@@ -1,4 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ nixpkgs_revision ? "c576998594b4b8790f291d17fa92d499d1dc5d42" }:
+let
+  pkgs = import (builtins.fetchTarball {
+    name = "nixpkgs-${nixpkgs_revision}";
+    url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs_revision}.tar.gz";
+    sha256 = "1k1xpardbbpb23wdki2ws30b3f20nd6fpx6lm802s1c6k3xh2d4c";
+  }) {};
+in
 pkgs.mkShell {
   buildInputs = [
     (import ./nix/code-generator.nix { inherit pkgs; })

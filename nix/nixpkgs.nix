2c54c1549cbb94b700b40a28a0f7f8e86a537777
Rafael Fernández López <ereslibre@ereslibre.es>
2021-06-02 22:41:15 +0200

    Bump nixpkgs to 21.05

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 {
   nixpkgs ? {
-    revision = "c576998594b4b8790f291d17fa92d499d1dc5d42";
-    sha256 = "1k1xpardbbpb23wdki2ws30b3f20nd6fpx6lm802s1c6k3xh2d4c";
+    revision = "7e9b0dff974c89e070da1ad85713ff3c20b0ca97";
+    sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
   }
 }:
 import (builtins.fetchTarball {

cf5c77a028c172d992bcd0ce5b018d41274ace6f
Rafael Fernández López <ereslibre@ereslibre.es>
2021-10-07 00:07:53 +0200

    Generate tooling image with nix

---
+++ images/tooling/default.nix
@@ -0,0 +1,14 @@
+let
+  pkgs = import <nixpkgs.nix> {};
+in pkgs.dockerTools.buildImage {
+  name = "tooling";
+  tag = "latest"; # FIXME (ereslibre)
+  contents = [
+    pkgs.coreutils
+    pkgs.dbus
+    (pkgs.writeScriptBin "write-base64-file.sh" ''
+      #!${pkgs.runtimeShell}
+      ${pkgs.coreutils}/bin/echo "$1" | ${pkgs.coreutils}/bin/base64 -d - > "$2"
+    '')
+  ];
+}
--- images/tooling/fs/usr/local/bin/write-base64-file.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-echo "$1" | base64 -d - > "$2"
